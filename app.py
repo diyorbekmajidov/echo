57d025882eea98bbf0fdbba579c81dbc9be7d0c1
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-14 12:04:57 +0500

    set webhook

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ TOKEN = os.environ['TOKEN']
 @app.route('/webhook', methods=['POST'])
 def webhook():
     # get data from request
-    data = print(request.get_json(force=True))
+    update = print(request.get_json(force=True))
+    text = update['message']['text']
+    chat_id = update['message']['chat']['id']
+    # send message
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+    r = requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage", params=payload)
 
     return 'ok'
---
+++ set_webhook.py
@@ -0,0 +1,10 @@
+import requests
+import os
+url = f"https://majidovdiyorbek.pythonanywhere.com/webhook"
+
+TOKEN = os.environ['TOKEN']
+payload = {
+    "url": url,
+}
+r = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook", params=payload)
+print(r.status_code)
